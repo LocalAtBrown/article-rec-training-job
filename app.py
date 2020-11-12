fd2d9dc9a08b8a430d817eb937690ca405c07485
ting zhang <thetingzhang@gmail.com>
2020-11-12 06:59:46 -0500

    run test query

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import logging
 
 from lib.config import config
+from lib.db import test_query
 
 
 def run_job():
     logging.info('Starting job...')
+    logging.info('Running test query...')
+    query_result = test_query()
+    logging.info(f'Got query result: {query_result}')
 
 
 if __name__ == '__main__':
---
+++ lib/db.py
@@ -0,0 +1,34 @@
+import logging
+
+import psycopg2
+
+from lib.config import config
+from lib.secrets_manager import get_secret
+
+
+DB_SECRET_KEY = config.get('DB_SECRET_KEY')
+DB_CONFIG = get_secret(DB_SECRET_KEY)
+PASSWORD = DB_CONFIG['password']
+NAME = DB_CONFIG['dbname']
+PORT = DB_CONFIG['port']
+ENDPOINT = DB_CONFIG['host']
+USER = DB_CONFIG['username']
+REGION = 'us-east-1'
+
+
+def test_query():
+    query_results = ''
+
+    try:
+        conn = psycopg2.connect(host=ENDPOINT,
+                                port=PORT,
+                                database=NAME,
+                                user=USER,
+                                password=PASSWORD)
+        cur = conn.cursor()
+        cur.execute("""SELECT now()""")
+        query_results = cur.fetchall()
+    except Exception:
+        logging.exception('query failed')
+
+    return query_results
