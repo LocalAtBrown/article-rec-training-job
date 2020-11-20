4961655365dd3f5289ad7a88e04e91b85cb25b4c
ting zhang <thetingzhang@gmail.com>
2020-11-20 10:56:33 -0500

    add job dir

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import logging
 
 from lib.config import config
-from lib.db import test_query
-
-
-def run_job():
-    logging.info('Starting job...')
-    logging.info('Running test query...')
-    query_result = test_query()
-    logging.info(f'Got query result: {query_result}')
+from job import job
 
 
 if __name__ == '__main__':
     log_level = config.get('LOG_LEVEL')
     logging.getLogger().setLevel(logging.getLevelName(log_level))
 
-    run_job()
+    job.run()
