0ba4165c8ec3e99a0e4fbb9a50e9317cfc0aeb2c
ting zhang <thetingzhang@gmail.com>
2021-01-15 16:43:12 -0500

    [Fix] drop and recreate

--- db/migrations/V0002__rename_recommendation_cols.sql
+++ db/migrations/V0002__rename_recommendation_cols.sql
@@ -1,6 +1,17 @@
-ALTER TABLE recommendation
-    RENAME COLUMN article_id TO recommended_article_id,
-    DROP COLUMN external_id,
-    ADD COLUMN source_article_id INTEGER NOT NULL REFERENCES article (id) ON DELETE CASCADE AFTER model_id,
-    ADD UNIQUE (source_article_id, recommended_article_id, model_id),
-    ADD INDEX idx_source_article_id_model_id ON recommendation (source_article_id, model_id);
+DROP TABLE recommendation;
+
+CREATE TABLE recommendation (
+  id SERIAL PRIMARY KEY,
+  model_id INTEGER NOT NULL REFERENCES model (id) ON DELETE CASCADE,
+  -- represents the entity being recommended for. could be an article, user, etc
+  source_entity_id TEXT NOT NULL,
+  -- represents the article recommended for the source entity
+  recommended_article_id INTEGER NOT NULL REFERENCES article (id) ON DELETE CASCADE,
+  -- how relevant the article is for the entity
+  score DECIMAL (7, 6) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  UNIQUE (model_id, source_entity_id, recommended_article_id)
+);
+
+CREATE INDEX idx_source_entity_id_model_id ON recommendation (source_entity_id, model_id);
