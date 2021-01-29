32cc5cdb0edbaf73ec402adc37e43501d484a5e5
Eric Bolton <edbltn@gmail.com>
2021-01-29 16:36:21 -0500

    [Remove] console logs

--- sites/wcp_tamper_monkey.js
+++ sites/wcp_tamper_monkey.js
@@ -13,14 +13,10 @@
     var title = recentPosts.getElementsByTagName("h2")[0];
     var recs = recentPosts.getElementsByTagName("a");
     title.getElementsByTagName("span")[0].innerHTML = "People Also Read";
-    console.log(recentPosts);
-    console.log(title);
-    console.log(recs);
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(json => {
         for (var i = 0; i < recs.length; i++) {
-            console.log(recs[i].outerHTML);
             recs[i].innerHTML = json.title;
         }
     })
