7d828acd34b430a56215bf992eef99a2ccb07acc
ting zhang <thetingzhang@gmail.com>
2021-02-11 16:27:03 -0500

    [Add] test

---
+++ tests/test_helpers.py
@@ -0,0 +1,42 @@
+import unittest
+from datetime import datetime
+
+import pandas as pd
+
+from job import helpers
+
+
+def generate_row(client_id, external_id):
+    return {
+        "client_id": client_id,
+        "external_id": external_id,
+        "event_category": "pageview",
+        "event_action": "pageview",
+        "session_date": datetime.now(),
+        "activity_time": datetime.now(),
+    }
+
+
+class TestHelpers(unittest.TestCase):
+    def test_calculate_default_recs(self) -> None:
+        client_a = "client.a"
+        client_b = "client.b"
+        article_a = 123
+        article_b = 456
+
+        data = [
+            # both clients read article a once
+            generate_row(client_a, article_a),
+            generate_row(client_b, article_a),
+            # client b read article b three times
+            generate_row(client_b, article_b),
+            generate_row(client_b, article_b),
+            generate_row(client_b, article_b),
+        ]
+
+        df = pd.DataFrame(data)
+        top_pageviews = helpers.calculate_default_recs(df)
+        # expect article a to be ranked higher than article b
+        assert all(top_pageviews.index == [article_a, article_b])
+        # expect article a to have two unique pageviews, and article b to have one
+        assert all(top_pageviews == [2, 1])
