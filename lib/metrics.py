eb818309284a71a46de2474a37dde008a7876017
ting zhang <thetingzhang@gmail.com>
2021-03-10 18:08:18 -0500

    [Change] reference unit class in default params

--- lib/metrics.py
+++ lib/metrics.py
@@ -9,33 +9,6 @@ client = boto3.client("cloudwatch", REGION)
 SERVICE = config.get("SERVICE")
 
 
-def write_metric(
-    name: str,
-    value: float,
-    unit: str = "Count",
-    tags: Dict[str, str] = None,
-) -> None:
-    if STAGE == "local":
-        logging.info("Skipping metric write")
-        return
-    default_tags = {"stage": STAGE}
-    if tags:
-        default_tags.update(tags)
-    formatted_tags = [{"Name": k, "Value": v} for k, v in default_tags.items()]
-
-    client.put_metric_data(
-        Namespace=SERVICE,
-        MetricData=[
-            {
-                "MetricName": name,
-                "Dimensions": formatted_tags,
-                "Value": value,
-                "Unit": unit,
-            },
-        ],
-    )
-
-
 class Unit:
     SECONDS = "Seconds"
     MICROSECONDS = "Microseconds"
@@ -63,3 +36,30 @@ class Unit:
     GIGABITS_PER_SECOND = "Gigabits/Second"
     TERABITS_PER_SECOND = "Terabits/Second"
     COUNT_PER_SECOND = "Count/Second"
+
+
+def write_metric(
+    name: str,
+    value: float,
+    unit: str = Unit.COUNT,
+    tags: Dict[str, str] = None,
+) -> None:
+    if STAGE == "local":
+        logging.info("Skipping metric write")
+        return
+    default_tags = {"stage": STAGE}
+    if tags:
+        default_tags.update(tags)
+    formatted_tags = [{"Name": k, "Value": v} for k, v in default_tags.items()]
+
+    client.put_metric_data(
+        Namespace=SERVICE,
+        MetricData=[
+            {
+                "MetricName": name,
+                "Dimensions": formatted_tags,
+                "Value": value,
+                "Unit": unit,
+            },
+        ],
+    )
