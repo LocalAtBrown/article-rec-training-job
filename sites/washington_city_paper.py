04e4e7a8cadb0a13de3af4d5caafbb6c86932ab4
Eric Bolton <edbltn@gmail.com>
2021-04-12 12:51:18 -0400

    [Add] more valid URLs to WCP regex

--- sites/washington_city_paper.py
+++ sites/washington_city_paper.py
@@ -6,21 +6,21 @@ from bs4 import BeautifulSoup
 from sites.helpers import safe_get, BadArticleFormatError
 
 
-PATH_PATTERN = "\/article\/(\d+)\/\S+"
+DOMAIN = "washingtoncitypaper.com"
+PATH_PATTERN = f"\/((v|c)\/s\/{DOMAIN}\/)?article\/(\d+)\/\S+"
 PATH_PROG = re.compile(PATH_PATTERN)
 
 
 def extract_external_id(path: str) -> int:
     result = PATH_PROG.match(path)
     if result:
-        return int(result.groups()[0])
+        return int(result.groups()[2])
     else:
         return None
 
 
 def scrape_article_metadata(path: str) -> dict:
-    DOMAIN = "https://washingtoncitypaper.com"
-    url = f"{DOMAIN}{path}"
+    url = f"https://{DOMAIN}{path}"
     logging.info(f"Scraping metadata from: {url}")
     page = safe_get(url)
     soup = BeautifulSoup(page.text, features="html.parser")
