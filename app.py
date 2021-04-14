cacede212a043f4cc04d2009bd05e348b10861b4
Eric Bolton <edbltn@gmail.com>
2021-04-14 15:27:37 -0400

    [Remove] multiprocessing logging

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import logging
-from multiprocessing_logging import install_mp_handler
 
 from lib.config import config
 from job import job
@@ -8,6 +7,5 @@ from job import job
 if __name__ == "__main__":
     log_level = config.get("LOG_LEVEL")
     logging.getLogger().setLevel(logging.getLevelName(log_level))
-    install_mp_handler()
 
     job.run()
