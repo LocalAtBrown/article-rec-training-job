428367c6bbd5673ffe7df3e6b28e6222fc8fc566
ting zhang <thetingzhang@gmail.com>
2021-12-02 11:22:22 -0500

    Revert "[Debug] wcp title issue"
    This reverts commit f5be9e0a4d26b25b2f29c3c7ff3d05016779f560.

--- sites/washington_city_paper.py
+++ sites/washington_city_paper.py
@@ -3,7 +3,6 @@ import re
 import logging
 from urllib.parse import urlparse
 from requests.models import Response
-import time
 
 from bs4 import BeautifulSoup
 
@@ -133,7 +132,6 @@ def validate_article(external_id: int) -> (Response, BeautifulSoup, Optional[str
 
     try:
         page = safe_get(url)
-        time.sleep(5)
     except Exception as e:
         msg = f"Error fetching article url: {url}"
         logging.exception(msg)
