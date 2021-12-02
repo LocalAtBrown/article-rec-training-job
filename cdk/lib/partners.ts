32caa87610a92f7cff3ef2fc6c20122f157f4ccb
ting zhang <thetingzhang@gmail.com>
2021-12-02 16:47:12 -0500

    [Change] beef up cpu + mem for tt

--- cdk/lib/partners.ts
+++ cdk/lib/partners.ts
@@ -1,5 +1,7 @@
 import { Organization } from "./helpers"
 
+// find more cpu and memory options for fargate here:
+// https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
 const DEFAULT_CPU = 2048;
 const DEFAULT_MEM = 8192;
 
@@ -21,8 +23,8 @@ export const partners: Array<Organization> = [
   {
     orgName: "texas-tribune",
     pascalName: "TexasTribune",
-    cpu: DEFAULT_CPU,
-    memoryLimitMiB: DEFAULT_MEM,
+    cpu: 4096,
+    memoryLimitMiB: 30720,
     enabled: false,
   },
 
