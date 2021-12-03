2d08ef400aa8c421f5771b8a7acfa8ccdce5af18
Sam Petulla <sam.petulla@gmail.com>
2021-12-03 12:58:20 -0500

    Uncomment warehouse updating

--- job/job.py
+++ job/job.py
@@ -51,7 +51,7 @@ def run():
             article_df, on="external_id", lsuffix="_original", how="inner"
         )
 
-        #warehouse.update_dwell_times(data_df, EXPERIMENT_DT.date(), site)
+        warehouse.update_dwell_times(data_df, EXPERIMENT_DT.date(), site)
 
         data_df = preprocess.filter_activities(data_df)
         data_df = preprocess.filter_articles(data_df)
