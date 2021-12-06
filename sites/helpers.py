a51f373946d2039ad053278ced3b30a9d565e9a3
Sam Petulla <sam.petulla@gmail.com>
2021-12-06 16:03:59 -0500

    Uncomment warehouse; fix dictionary pointer

--- sites/helpers.py
+++ sites/helpers.py
@@ -36,7 +36,7 @@ class ArticleFetchError(Exception):
     pass
 
 @retry(stop_max_attempt_number=3, wait_exponential_multiplier=1000)
-def safe_get(url: str, headers: Dict[str, str] = None, params: Optional[Dict] = {}) -> str:
+def safe_get(url: str, headers: Dict[str, str] = None, params: Optional[Dict] = None) -> str:
     TIMEOUT_SECONDS = 30
     default_headers = {"User-Agent": "article-rec-training-job/1.0.0"}
     if headers:
