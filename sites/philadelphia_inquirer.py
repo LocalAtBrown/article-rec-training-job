8cd4b48f7186755f26a0ed107cb223d0398806dc
ting zhang <thetingzhang@gmail.com>
2022-02-01 16:00:03 -0500

    [add] sleep

--- sites/philadelphia_inquirer.py
+++ sites/philadelphia_inquirer.py
@@ -1,3 +1,4 @@
+import time
 from typing import Optional, List, Dict, Any, Union
 from requests.models import Response
 from datetime import datetime
@@ -59,6 +60,8 @@ def bulk_fetch(
     metadata = [
         parse_article_metadata(a, a["_id"]) for a in json_res["content_elements"]
     ]
+    # TODO replace with 1 / config-specified rps after it's merged
+    time.sleep(0.25)
     return metadata
 
 
