8b8cfe1cdfd65ebe3a4f1c6adab348932e21c6b1
ting <thetingzhang@gmail.com>
2022-03-02 11:46:30 -0500

    Fix empty title bug (#114)
    * [fix] use logical or for default title
    
    * [add] test for get_headline

---
+++ tests/sites/test_philadelphia_inquirer.py
@@ -0,0 +1,28 @@
+from copy import deepcopy
+
+from tests.base import BaseTest
+import sites.philadelphia_inquirer as site
+
+
+class TestPhiladelphiaInquirer(BaseTest):
+    def setUp(self) -> None:
+        res = {
+            "headlines": {
+                "meta_title": "Russia-Ukraine updates: Kremlin strikes Ukrainian city with suspected cluster munitions; peace talks end",
+                "basic": "Russia strikes Ukrainian city with suspected cluster munitions as peace talks end",
+            },
+            "publish_date": "2022-03-01T18:37:44.908Z",
+            "_id": "TCKTFPUVXJE5LJXG7THB4PLMKM",
+            "canonical_url": "/news/nation-world/live/russia-ukraine-conflict-crisis-20220228.html",
+        }
+        self.res = deepcopy(res)
+        super().setUp()
+
+    def test_get_headline(self) -> None:
+        title = site.get_headline(self.res)
+        assert title == self.res["headlines"]["meta_title"]
+
+    def test_get_headline__empty_meta_title(self) -> None:
+        self.res["headlines"]["meta_title"] = ""
+        title = site.get_headline(self.res)
+        assert title == self.res["headlines"]["basic"]
