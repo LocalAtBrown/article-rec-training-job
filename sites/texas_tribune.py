94e3d8ebb65db07cba5fc6b5c2c8e44b4bb231d0
Raaid <raaid@protonmail.com>
2022-08-09 11:35:48 +0100

    implement TT's bulk_fetch

--- sites/texas_tribune.py
+++ sites/texas_tribune.py
@@ -278,7 +278,21 @@ class TexasTribune(NewSite):
         return res
 
     def bulk_fetch(self, start_date: date, end_date: date) -> List[Dict[str, Any]]:
-        pass
+        logging.info(f"Fetching articles from {start_date} to {end_date}")
+
+        API_URL = f"https://{DOMAIN}/api/v2/articles"
+        DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+        # texas tribune publishes 5-10 articles per day
+        params = {
+            "start_date": start_date.strftime(DATE_FORMAT),
+            "end_date": end_date.strftime(DATE_FORMAT),
+            "limit": 100,
+        }
+        res = safe_get(API_URL, params=params, scrape_config=SCRAPE_CONFIG)
+        json_res = res.json()
+
+        metadata = [parse_metadata(article) for article in json_res["results"]]
+        return metadata
 
 
 NEW_TT_SITE = TexasTribune(
