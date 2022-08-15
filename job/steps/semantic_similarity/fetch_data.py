638799f7f67f42766e7cca54a870a1fc3696bcaa
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-08-15 11:51:33 -0400

    [change] bulk fetch by ID func accordingg to Liam's specs

--- job/steps/semantic_similarity/fetch_data.py
+++ job/steps/semantic_similarity/fetch_data.py
@@ -4,17 +4,13 @@ import pandas as pd
 
 from sites.site import Site
 
-# from typing import Set
-
 
 def run(site: Site, interactions_data: pd.DataFrame) -> List[Dict[str, Any]]:
     """
     Main script. Fetch data of all articles included in the `interactions_data` table.
     """
     # https://stackoverflow.com/questions/46839277/series-unique-vs-list-of-set-performance
-    # external_ids: Set[str] = set(interactions_data["external_id"])
-    # TODO: Remove next line and uncomment preceding line as soon as bulk_fetch_by_external_id() is rewritten following Liam's suggestions
-    external_ids = {"40933", "40929", "15", "2"}
+    external_ids: List[str] = list(set(interactions_data["external_id"]))
 
     data = site.bulk_fetch_by_article_id(external_ids)
     return data
