627cb574770138f6f2d47c18f1fae7f1f9da85a4
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-08-18 15:25:37 -0400

    [change] move unique ID extraction back to fetch_data.py

--- job/steps/semantic_similarity/fetch_data.py
+++ job/steps/semantic_similarity/fetch_data.py
@@ -1,18 +1,35 @@
 import logging
 from typing import Any, Dict, List
 
+import pandas as pd
+
 from sites.site import Site
 
 
-def run(site: Site, external_ids: List[str]) -> List[Dict[str, Any]]:
-    """
-    Fetches article data using site-specific ID-based bulk-fetching method, given a list of external IDs.
-    """
-    data = site.bulk_fetch_by_article_id(external_ids)
+def get_external_ids(interactions_data: pd.DataFrame) -> List[str]:
+    # https://stackoverflow.com/questions/46839277/series-unique-vs-list-of-set-performance
+    return list(set(interactions_data["external_id"]))
 
-    num_to_fetch = len(external_ids)
-    num_fetched = len(data)
+
+def validate(data_fetched: List[Dict[str, Any]], external_ids_to_fetch: List[str]) -> None:
+    num_to_fetch = len(external_ids_to_fetch)
+    num_fetched = len(data_fetched)
+
     if num_to_fetch != num_fetched:
         logging.warning(f"No. articles fetched ({num_fetched}) doesn't match no. articles to fetch ({num_to_fetch}).")
+
+
+def run(site: Site, interactions_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Main script. Fetch data of all articles included in the `interactions_data` table.
+    """
+    # Grab unique external IDs from interactions table
+    external_ids = get_external_ids(interactions_data)
+
+    # Fetch article data using said IDs
+    data: List[Dict[str, Any]] = site.bulk_fetch_by_article_id(external_ids)
+
+    # Validation
+    validate(data, external_ids)
 
     return data
