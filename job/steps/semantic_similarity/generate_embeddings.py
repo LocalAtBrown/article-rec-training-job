bce77c2aa7144bdc06a15f6a6a534f44247e482d
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-08-22 16:14:04 -0400

    [change] move encode arg to its correct place

--- job/steps/semantic_similarity/generate_embeddings.py
+++ job/steps/semantic_similarity/generate_embeddings.py
@@ -19,6 +19,6 @@ def run(site: Site, data: List[Dict[str, Any]], pretrained_model_name: str) -> n
     Given fetched article data with at least one text field, create article-level text embeddings.
     """
     texts = get_article_text_representations(site, data)
-    model = create_model(pretrained_model_name, convert_to_numpy=True)
+    model = create_model(pretrained_model_name)
 
-    return model.encode(texts)
+    return model.encode(texts, convert_to_numpy=True)
