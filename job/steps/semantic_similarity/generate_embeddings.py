a84ba18b20d488f5b31490522d4699eb6dbd2feb
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-08-22 16:39:11 -0400

    [remove] cleanup

--- job/steps/semantic_similarity/generate_embeddings.py
+++ job/steps/semantic_similarity/generate_embeddings.py
@@ -6,19 +6,11 @@ from sentence_transformers import SentenceTransformer
 from sites.site import Site
 
 
-def get_article_text_representations(site: Site, data: List[Dict[str, Any]]) -> List[str]:
-    return [site.get_article_text(article) for article in data]
-
-
-def create_model(pretrained_model_name: str) -> SentenceTransformer:
-    return SentenceTransformer(pretrained_model_name, device="cpu")
-
-
 def run(site: Site, data: List[Dict[str, Any]], pretrained_model_name: str) -> np.ndarray:
     """
     Given fetched article data with at least one text field, create article-level text embeddings.
     """
-    texts = get_article_text_representations(site, data)
-    model = create_model(pretrained_model_name)
+    texts = [site.get_article_text(article) for article in data]
+    model = SentenceTransformer(pretrained_model_name, device="cpu")
 
     return model.encode(texts, convert_to_numpy=True)
