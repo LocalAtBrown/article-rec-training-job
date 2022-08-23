6037ce6cfc5f0c01005a9dda67c80379232db3e0
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-08-23 17:58:59 -0400

    [add] comments and interactions dropna

--- job/steps/semantic_similarity/fetch_data.py
+++ job/steps/semantic_similarity/fetch_data.py
@@ -16,7 +16,7 @@ def validate(data_fetched: List[Dict[str, Any]], external_ids_to_fetch: List[str
 
 def run(site: Site, interactions_data: pd.DataFrame) -> pd.DataFrame:
     """
-    Main script. Fetch data of all articles included in the `interactions_data` table.
+    Fetch text data of all articles included in the `interactions_data` table and join them with article ID metadata values.
     """
     # Grab unique external IDs from interactions table
     # https://stackoverflow.com/questions/46839277/series-unique-vs-list-of-set-performance
@@ -28,12 +28,13 @@ def run(site: Site, interactions_data: pd.DataFrame) -> pd.DataFrame:
     # Validation
     validate(data_fetched, external_ids)
 
+    # From now on everything is preprocessing
     # Create text representation of each article
     data = [{"external_id": article["external_id"], "text": site.get_article_text(article)} for article in data_fetched]
     df_data = pd.DataFrame(data, dtype=str)
 
     # Extract article metadata (LNL DB article ID, categorial item ID, publish date) from interactions_data
-    df_metadata = interactions_data[["article_id", "external_id", "published_at"]].drop_duplicates()
+    df_metadata = interactions_data[["article_id", "external_id", "published_at"]].dropna().drop_duplicates()
 
     # Merge article metadata with article text representations; the result is a DataFrame with 4 columns:
     # article_id, external_id, published_at and text
