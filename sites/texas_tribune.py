4c00540d1b5cbcf152e55ae51a6c3f1379e32a23
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-09-02 16:22:40 -0400

    Squashed commit of the following:
    commit 57391ea42607a6e662b8900672b971bbbc339439
    Author: Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
    Date:   Fri Sep 2 16:18:35 2022 -0400
    
        [change] appease mypy

--- sites/texas_tribune.py
+++ sites/texas_tribune.py
@@ -100,7 +100,7 @@ def get_article_text(metadata: Dict[str, Any]) -> str:
 
 
 # Added to accommodate SS
-def batch_fetch_by_external_id(batch_external_ids: List[str]) -> Dict[str, Any]:
+def batch_fetch_by_external_id(batch_external_ids: List[str]) -> List[Dict[str, Any]]:
     """
     Helper for bulk_fetch_by_external_id. Sends a request for a batch of IDs.
     """
