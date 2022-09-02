7c272f6679040e9565a98ddb8f7b9080d59fcee2
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-09-02 13:06:05 -0400

    [add] comments

--- job/steps/semantic_similarity.py
+++ job/steps/semantic_similarity.py
@@ -10,11 +10,14 @@ def fetch_data(site: Site, interactions_data: pd.DataFrame) -> List[Dict[str, An
     """
     Main script. Fetch data of all articles included in the `interactions_data` table.
     """
+    # Grab unique external IDs from Interactions table
     # https://stackoverflow.com/questions/46839277/series-unique-vs-list-of-set-performance
     external_ids: List[str] = list(set(interactions_data["external_id"]))
 
+    # Fetch article text data using said IDs
     data = site.bulk_fetch_by_external_id(external_ids)
 
+    # Validation: check if all external IDs have been fetched
     num_to_fetch = len(external_ids)
     num_fetched = len(data)
     if num_to_fetch != num_fetched:
