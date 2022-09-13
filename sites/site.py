cf1342d0942234350779f914993f137330392acc
Raaid <raaid@protonmail.com>
2022-09-13 12:37:34 -0400

    Update site object (#149)
    * add first pass of a new Site class skeleton
    
    * add singleton metaclass definition
    
    * add fleshed out function headers for abstract methods in NewSite
    
    * update singleton to singletonabcmeta
    
    * use SingeltonABCMeta instead of ABC for the new site object's metaclass
    
    * add skeleton for TexasTribune subclass and instantiation
    
    * implement TT's transform_raw_data
    
    * implement TT's extract_external_id
    
    * implement TT's scrape_article_metadata
    
    * implement TT's fetch_article
    
    * implement TT's bulk_fetch
    
    * refer to self.scrape_config instead of SCRAPE_CONFIG var
    
    * move some helper functions into the class as static methods
    
    * update wcp to NewSite, fix typing for `fields` field in `Site`
    
    * update philadelphia inquirer to new site object
    
    * get tests to pass, a dev run also worked now that we swapped out the old Site object for the new one!
    
    * remove commented-out old Site object
    
    * remove extra get_bucket_name function and its one use, use Site's method instead
    
    * change transform_raw_data to a static method, update it's downstream implementations too
    
    * add warning message when trying to instantiate more than one instance of a singleton
    
    * [add] comment referencing PR
    
    Co-authored-by: Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>

--- sites/site.py
+++ sites/site.py
@@ -1,24 +1,54 @@
-from collections import namedtuple
+from abc import abstractmethod
+from dataclasses import dataclass
+from datetime import date
+from typing import Any, Dict, List, Optional, Set, Union
 
-Site = namedtuple(
-    "Site",
-    [
-        "name",
-        "fields",
-        "training_params",
-        "scrape_config",
-        "transform_raw_data",
-        "extract_external_id",
-        "scrape_article_metadata",
-        "fetch_article",
-        "bulk_fetch",
-        "bulk_fetch_by_external_id",
-        "get_article_text",
-        "popularity_window",
-        "max_article_age",
-    ],
-)
+import pandas as pd
+from requests.models import Response
+
+from sites.singleton import SingletonABCMeta
 
 
-def get_bucket_name(site: Site):
-    return f"lnl-snowplow-{site.name}"
+@dataclass
+class Site(metaclass=SingletonABCMeta):
+    name: str
+    fields: Set[str]  # needs better name
+    training_params: dict  # should define with more specifics unless it needs to be flexible
+    scrape_config: dict  # should define with more specifics unless it needs to be flexible
+    # this is a number of days; will only recommend articles within the past X days
+    popularity_window: int
+    # this is a number of years; will grab dwell time data for any article within the past X years
+    max_article_age: int
+
+    def get_bucket_name(self):
+        return f"lnl-snowplow-{self.name}"
+
+    @abstractmethod
+    def extract_external_id(self, path: str) -> Optional[str]:
+        pass
+
+    @abstractmethod
+    def scrape_article_metadata(self, page: Union[Response, dict], external_id: str, path: str) -> dict:
+        pass
+
+    @abstractmethod
+    def fetch_article(self, external_id: str, path: str) -> Response:
+        pass
+
+    @abstractmethod
+    def bulk_fetch(self, start_date: date, end_date: date) -> List[Dict[str, Any]]:
+        pass
+
+    @abstractmethod
+    def bulk_fetch_by_external_id(self, external_ids: List[str]) -> List[Dict[str, Any]]:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def transform_raw_data(df: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_article_text(metadata: Dict[str, Any]) -> str:
+        pass
--- sites/washington_city_paper.py
+++ sites/washington_city_paper.py
@@ -1,6 +1,6 @@
 import re
 from datetime import date
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 from urllib.parse import urlparse
 
 import pandas as pd
@@ -50,134 +50,126 @@ PATH_PATTERN = rf"\/((v|c)\/s\/{DOMAIN}\/)?article\/(\d+)\/\S+"
 PATH_PROG = re.compile(PATH_PATTERN)
 
 
-def bulk_fetch(start_date: date, end_date: date) -> List[Dict[str, Any]]:
-    raise NotImplementedError
+class WashingtonCityPaper(Site):
+    def extract_external_id(self, path: str) -> Optional[str]:
+        result = PATH_PROG.match(path)
+        if result:
+            return result.groups()[2]
+        else:
+            raise ArticleScrapingError(
+                ScrapeFailure.NO_EXTERNAL_ID, path, external_id=None, msg="External ID not found in path"
+            )
 
+    def scrape_article_metadata(self, page: Union[Response, dict], external_id: str, path: str) -> dict:
+        soup = BeautifulSoup(page.text, features="html.parser")
+        metadata = {}
+        scraper_funcs = [
+            ("title", self.scrape_title),
+            ("published_at", self.scrape_published_at),
+            ("path", self.scrape_path),
+        ]
 
-def bulk_fetch_by_external_id() -> None:
-    # https://stackoverflow.com/questions/16706956/is-there-a-difference-between-raise-exception-and-raise-exception-without
-    raise NotImplementedError
+        for prop, func in scraper_funcs:
+            try:
+                val = func(page, soup)
+            except Exception as e:
+                raise ArticleScrapingError(
+                    ScrapeFailure.MALFORMED_RESPONSE, path, external_id, f"Error scraping {prop} for article path"
+                ) from e
+            metadata[prop] = val
 
+        return metadata
 
-def get_article_text() -> None:
-    raise NotImplementedError
+    def fetch_article(self, external_id: str, path: str) -> Response:
+        url = f"https://{DOMAIN}{path}"
 
-
-def transform_raw_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    requires a dataframe with the following fields:
-    - contexts_dev_amp_snowplow_amp_id_1
-    - collector_tstamp
-    - page_urlpath
-    returns a dataframe with the following fields:
-    - client_id
-    - session_date
-    - activity_time
-    - landing_page_path
-    - event_category (conversions, newsletter sign-ups TK)
-    - event_action (conversions, newsletter sign-ups TK)
-    """
-    df = df.dropna(subset=["contexts_dev_amp_snowplow_amp_id_1"])
-    transformed_df = pd.DataFrame()
-    transformed_df["client_id"] = df.contexts_dev_amp_snowplow_amp_id_1.apply(lambda x: x[0]["ampClientId"])
-    transformed_df["activity_time"] = pd.to_datetime(df.collector_tstamp).dt.round("1s")
-    transformed_df["session_date"] = pd.to_datetime(transformed_df.activity_time.dt.date)
-    transformed_df["landing_page_path"] = df.page_urlpath
-    transformed_df["event_name"] = df.event_name
-    transformed_df.replace({"event_name": "amp_page_ping"}, Event.PAGE_PING.value, inplace=True)
-    transformed_df["event_name"] = transformed_df["event_name"].astype("category")
-
-    return transformed_df
-
-
-def extract_external_id(path: str) -> str:
-    result = PATH_PROG.match(path)
-    if result:
-        return result.groups()[2]
-    else:
-        raise ArticleScrapingError(
-            ScrapeFailure.NO_EXTERNAL_ID, path, external_id=None, msg="External ID not found in path"
-        )
-
-
-def scrape_title(page: Response, soup: BeautifulSoup) -> str:
-    headers = soup.select("header h1")
-    return headers[0].text.strip()
-
-
-def scrape_published_at(page: Response, soup: BeautifulSoup) -> str:
-    # example published_at: '2021-04-13T19:00:45+00:00'
-    PROPERTY_TAG = "article:published_time"
-    tag = soup.find("meta", property=PROPERTY_TAG)
-    return tag.get("content") if tag is not None else None
-
-
-def scrape_path(page: Response, soup: BeautifulSoup) -> str:
-    # there are times when older articles redirect to an alternate path, for ex:
-    # https://washingtoncitypaper.com/food/article/20830693/awardwinning-chef-michel-richard-dies-at-age-68
-    return urlparse(page.url).path
-
-
-def scrape_article_metadata(page: Response, external_id: str, path: str) -> dict:
-    soup = BeautifulSoup(page.text, features="html.parser")
-    metadata = {}
-    scraper_funcs = [
-        ("title", scrape_title),
-        ("published_at", scrape_published_at),
-        ("path", scrape_path),
-    ]
-
-    for prop, func in scraper_funcs:
         try:
-            val = func(page, soup)
+            page = safe_get(url)
         except Exception as e:
             raise ArticleScrapingError(
-                ScrapeFailure.MALFORMED_RESPONSE, path, external_id, f"Error scraping {prop} for article path"
+                ScrapeFailure.FETCH_ERROR, path, str(external_id), f"Request failed for {url}"
             ) from e
-        metadata[prop] = val
 
-    return metadata
+        error_msg = validate_response(page, [validate_status_code, self.validate_not_excluded])
+        if error_msg is not None:
+            raise ArticleScrapingError(ScrapeFailure.FAILED_SITE_VALIDATION, path, str(external_id), error_msg)
+
+        return page
+
+    def bulk_fetch_by_external_id(self, external_ids) -> None:
+        # https://stackoverflow.com/questions/16706956/is-there-a-difference-between-raise-exception-and-raise-exception-without
+        raise NotImplementedError
+
+    @staticmethod
+    def transform_raw_data(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        requires a dataframe with the following fields:
+        - contexts_dev_amp_snowplow_amp_id_1
+        - collector_tstamp
+        - page_urlpath
+        returns a dataframe with the following fields:
+        - client_id
+        - session_date
+        - activity_time
+        - landing_page_path
+        - event_category (conversions, newsletter sign-ups TK)
+        - event_action (conversions, newsletter sign-ups TK)
+        """
+        df = df.dropna(subset=["contexts_dev_amp_snowplow_amp_id_1"])
+        transformed_df = pd.DataFrame()
+        transformed_df["client_id"] = df.contexts_dev_amp_snowplow_amp_id_1.apply(lambda x: x[0]["ampClientId"])
+        transformed_df["activity_time"] = pd.to_datetime(df.collector_tstamp).dt.round("1s")
+        transformed_df["session_date"] = pd.to_datetime(transformed_df.activity_time.dt.date)
+        transformed_df["landing_page_path"] = df.page_urlpath
+        transformed_df["event_name"] = df.event_name
+        transformed_df.replace({"event_name": "amp_page_ping"}, Event.PAGE_PING.value, inplace=True)
+        transformed_df["event_name"] = transformed_df["event_name"].astype("category")
+
+        return transformed_df
+
+    @staticmethod
+    def get_article_text(metadata: Dict[str, Any]) -> None:
+        raise NotImplementedError
+
+    def bulk_fetch(self, start_date: date, end_date: date) -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+    @staticmethod
+    def validate_not_excluded(page: Response) -> Optional[str]:
+        soup = BeautifulSoup(page.text, features="html.parser")
+        primary = soup.find(id="primary")
+
+        if primary:
+            classes = {value for element in primary.find_all(class_=True) for value in element["class"]}
+            if "tag-exclude" in classes:
+                return "Article has exclude tag"
+
+        return None
+
+    @staticmethod
+    def scrape_path(page: Response, soup: BeautifulSoup) -> str:
+        # there are times when older articles redirect to an alternate path, for ex:
+        # https://washingtoncitypaper.com/food/article/20830693/awardwinning-chef-michel-richard-dies-at-age-68
+        return urlparse(page.url).path
+
+    @staticmethod
+    def scrape_title(page: Response, soup: BeautifulSoup) -> str:
+        headers = soup.select("header h1")
+        return headers[0].text.strip()
+
+    @staticmethod
+    def scrape_published_at(page: Response, soup: BeautifulSoup) -> str:
+        # example published_at: '2021-04-13T19:00:45+00:00'
+        PROPERTY_TAG = "article:published_time"
+        tag = soup.find("meta", property=PROPERTY_TAG)
+        return tag.get("content") if tag is not None else None
 
 
-def validate_not_excluded(page: Response) -> Optional[str]:
-    soup = BeautifulSoup(page.text, features="html.parser")
-    primary = soup.find(id="primary")
-
-    if primary:
-        classes = {value for element in primary.find_all(class_=True) for value in element["class"]}
-        if "tag-exclude" in classes:
-            return "Article has exclude tag"
-
-    return None
-
-
-def fetch_article(external_id: str, path: str) -> Response:
-    url = f"https://{DOMAIN}{path}"
-
-    try:
-        page = safe_get(url)
-    except Exception as e:
-        raise ArticleScrapingError(ScrapeFailure.FETCH_ERROR, path, str(external_id), f"Request failed for {url}") from e
-
-    error_msg = validate_response(page, [validate_status_code, validate_not_excluded])
-    if error_msg is not None:
-        raise ArticleScrapingError(ScrapeFailure.FAILED_SITE_VALIDATION, path, str(external_id), error_msg)
-
-    return page
-
-
-WCP_SITE = Site(
-    NAME,
-    FIELDS,
-    TRAINING_PARAMS,
-    SCRAPE_CONFIG,
-    transform_raw_data,
-    extract_external_id,
-    scrape_article_metadata,
-    fetch_article,
-    bulk_fetch,
-    bulk_fetch_by_external_id,
-    get_article_text,
-    POPULARITY_WINDOW,
-    MAX_ARTICLE_AGE,
+WCP_SITE = WashingtonCityPaper(
+    name=NAME,
+    fields=FIELDS,
+    training_params=TRAINING_PARAMS,
+    scrape_config=SCRAPE_CONFIG,
+    popularity_window=POPULARITY_WINDOW,
+    max_article_age=MAX_ARTICLE_AGE,
 )
