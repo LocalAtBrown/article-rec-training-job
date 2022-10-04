89dd0f96a73287f9114208a3587bb6f989b89fa6
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-10-04 12:20:06 -0400

    [add] comments

--- sites/strategy.py
+++ sites/strategy.py
@@ -9,8 +9,11 @@ class Strategy(Enum):
     TODO: Unify with db/model.py?
     """
 
+    # Article-based collaborative filtering (as opposed to user-based, which is mentioned in db/model.py
+    # but never used and generally have gone out of favor)
     COLLABORATIVE_FILTERING = "collaborative_filtering"
     SEMANTIC_SIMILARITY = "semantic_similarity"
     POPULARITY = "popularity"  # Default popularity approach as fallback. Will always be run.
-    # Add new approaches here (which may use one or more models/model configs, e.g., SS, CF)
+    # Add new approaches here (which may use one or more models/model configs; for example,
+    # an SS-CF hybrid requires both SS config and CF config)
     # e.g., HYBRID_SS_CF = "hybrid_ss_cf"
