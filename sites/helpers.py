78b05db3626fbf3477117a8b8fb27ae9dd42827f
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-10-31 16:04:12 -0400

    [change] move raise_for_status before sleep

--- sites/helpers.py
+++ sites/helpers.py
@@ -82,8 +82,6 @@ def safe_get(
     if headers:
         default_headers.update(headers)
     page = req.get(url, timeout=TIMEOUT_SECONDS, params=params, headers=default_headers)
-    if scrape_config.get("requests_per_second"):
-        time.sleep(1 / scrape_config["requests_per_second"])
 
     # Many times, the request hits a 4xx or 5xx, but no exception is raised
     # This makes sure an exception is raised and allows the retry decorator to work.
@@ -93,6 +91,9 @@ def safe_get(
     # - During the last retry attempt, if this still raises an exception, that exception's already handled
     # by a try-except block in each site's fetch_article method.
     page.raise_for_status()
+
+    if scrape_config.get("requests_per_second"):
+        time.sleep(1 / scrape_config["requests_per_second"])
 
     return page
 
