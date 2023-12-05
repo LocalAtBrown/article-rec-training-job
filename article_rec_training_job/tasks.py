71c1939a0df275a4ebbfd3a0afe474a55b6e0033
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-12-04 20:37:32 -0500

    [add] page-fetching step in UpdatePages

--- article_rec_training_job/tasks.py
+++ article_rec_training_job/tasks.py
@@ -4,7 +4,11 @@ from datetime import date, datetime
 from typing import Protocol
 
 import pandera as pa
+from article_rec_db.models import Page
 from loguru import logger
+from pydantic import HttpUrl
+from sqlalchemy.engine import Engine
+from sqlmodel import Session
 
 from article_rec_training_job.shared.types.event_fetchers import (
     OutputDataFrame as FetchedEventsDataFrame,
@@ -27,7 +31,11 @@ class EventFetcher(Protocol):
 
 
 class PageFetcher(Protocol):
-    raise NotImplementedError
+    def fetch(self, urls: list[HttpUrl]) -> list[Page]:
+        ...
+
+    def post_fetch(self) -> None:
+        ...
 
 
 # ----- TASKS BASE CLASSES -----
@@ -40,27 +48,49 @@ class Task(ABC):
 class FetchesEvents:
     @staticmethod
     @pa.check_types
-    def fetch_events(event_fetcher: EventFetcher) -> FetchedEventsDataFrame:
-        df = event_fetcher.fetch()
-        event_fetcher.post_fetch()
+    def fetch_events(fetcher: EventFetcher) -> FetchedEventsDataFrame:
+        df = fetcher.fetch()
+        fetcher.post_fetch()
         return df
+
+
+class FetchesPages:
+    @staticmethod
+    def fetch_pages(fetcher: PageFetcher, urls: list[HttpUrl]) -> list[Page]:
+        pages = fetcher.fetch(urls)
+        fetcher.post_fetch()
+        return pages
 
 
 # ----- TASKS -----
 @dataclass
-class UpdatePages(Task, FetchesEvents):
+class UpdatePages(Task, FetchesEvents, FetchesPages):
     execution_timestamp: datetime
     event_fetcher: EventFetcher
-    # page_fetcher: PageFetcher
+    page_fetcher: PageFetcher
+    sa_engine: Engine
 
     def execute(self) -> None:
+        # First, fetch events
         logger.info(
             f"Fetching events from {self.event_fetcher.date_start} to {self.event_fetcher.date_end}",
             "to find pages to update",
         )
         df = self.fetch_events(self.event_fetcher)
-        page_urls = set(df[FetchEventsSchema.page_url])
-        logger.info(f"Fetched {len(df)} events and found {len(page_urls)} unique page URLs")
+        logger.info(f"Fetched {len(df)} events")
+
+        # Then, fetch pages.
+        # The fetcher is responsible for identifying which pages to fetch anew/create and which to update,
+        # as well as including an Article object in a Page object that corresponds to the page's article.
+        page_urls = [*set(df[FetchEventsSchema.page_url])]
+        logger.info(f"Found {len(page_urls)} URLs from events")
+        pages = self.fetch_pages(self.page_fetcher, page_urls)
+
+        # Finally, update pages
+        with Session(self.sa_engine) as session:
+            # If a page has an article, that article is updated/created as well
+            session.add_all(pages)
+            session.commit()
 
 
 @dataclass
