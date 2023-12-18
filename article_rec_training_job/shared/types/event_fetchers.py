501ea87707df0549e283f05a5009f7197dcae26e
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-12-18 15:00:46 -0500

    [change] ga4 event fetcher to emit metrics dataframe

--- article_rec_training_job/shared/types/event_fetchers.py
+++ article_rec_training_job/shared/types/event_fetchers.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Annotated, TypeAlias
 
 import pandera as pa
@@ -17,6 +18,22 @@ class OutputSchema(pa.DataFrameModel):
     user_id: str
     engagement_time_msec: int = pa.Field(nullable=True)
     page_url: str
+
+
+@dataclass
+class Metrics:
+    """
+    Reporting metrics an event fetcher must provide.
+    """
+
+    num_tables_exist: int
+    num_queries_executed: int
+    num_queries_use_cache: int
+    time_taken_to_find_tables: float
+    time_taken_to_fetch_events: float
+    total_bytes_fetched: int
+    total_bytes_billed: int
+    total_rows_fetched: int
 
 
 OutputDataFrame: TypeAlias = DataFrame[OutputSchema]
