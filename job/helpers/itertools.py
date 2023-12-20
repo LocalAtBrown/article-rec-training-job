7f7863d1ea82515285e0e400677e231be768a374
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-12-20 17:14:34 -0500

    [remove] legacy stuff

--- job/helpers/itertools.py
+++
@@ -1,4 +0,0 @@
-def batch(iterable, n=1):
-    total_length = len(iterable)
-    for ndx in range(0, total_length, n):
-        yield iterable[ndx : min(ndx + n, total_length)]
--- job/helpers/site.py
+++
@@ -1,9 +0,0 @@
-from sites.sites import Sites
-from sites.templates.site import Site
-
-
-def get_site(site_name) -> Site:
-    site = Sites.mapping.get(site_name)
-    if site is None:
-        raise Exception(f"Could not find site {site_name} in sites.py")
-    return site
