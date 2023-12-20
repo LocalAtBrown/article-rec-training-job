f293de9cc7a46b7687ae29d9fa3d766cb52df256
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-12-20 14:40:47 -0500

    Add WP page fetcher unit tests (#185)
    * [change] ga4 event fetcher to emit metrics dataframe
    
    * [change] wp page fetcher to emit metrics dataclass
    
    * [add] postgres page writer
    
    * [change] refactor tasks
    
    * [change] docs
    
    * [change] appease mypy
    
    * [add] bigquery dummy data for ga4 fetch test
    
    * [remove] cdk dir
    
    * [add] ga4 df test
    
    * [add] metrics test
    
    * [add] testing instruction
    
    * [add] comment
    
    * [add] project id fixture and comments
    
    * [add] page fetcher helper unit tests
    
    * [add] page fetcher helper tests
    
    * [add] notes on integration testing
    
    * [add] notes on integration testing
    
    * [add] compose.yaml, data and conftest.py to integration test dir
    
    * [add] testing to actions
    
    * [change] oops
    
    * [change] oops

--- article_rec_training_job/components/page_fetchers/helpers.py
+++ article_rec_training_job/components/page_fetchers/helpers.py
@@ -1,8 +1,10 @@
+import re
 from enum import Enum
 from urllib.parse import urlunparse
 
 import nh3
 from aiohttp import ClientResponse, ClientSession
+from article_rec_db.models.article import Language
 from loguru import logger
 from pydantic import HttpUrl
 from tenacity import (
@@ -37,6 +39,41 @@ def clean_url(url: HttpUrl) -> HttpUrl:
     """
     url_new = build_url(scheme=url.scheme, host=url.host, path=url.path)  # type: ignore
     return url_new
+
+
+def infer_language_from_url(patterns_language: dict[Language, re.Pattern[str]], url: HttpUrl) -> Language:
+    """
+    Infer content language from a given URL.
+    Returns English as a fallback.
+    """
+    path = url.path
+    for language, pattern in patterns_language.items():
+        if pattern.fullmatch(path) is not None:  # type: ignore
+            return language
+    return Language.ENGLISH
+
+
+def extract_slug_from_url(pattern_slug: re.Pattern[str], url: HttpUrl) -> str | None:
+    """
+    Grabs the slug from a given URL.
+    """
+    path = url.path
+
+    slug_match = pattern_slug.fullmatch(path)  # type: ignore
+    if slug_match is None:
+        logger.info(
+            f"Could not extract slug from path {path}. It will be considered a non-article page.",
+        )
+        return None
+
+    return slug_match.group("slug")
+
+
+def remove_urls_with_invalid_prefix(correct_url_prefix: HttpUrl, urls: set[HttpUrl]) -> set[HttpUrl]:
+    """
+    Removes URLs with invalid prefix.
+    """
+    return {url for url in urls if url.scheme == correct_url_prefix.scheme and url.host == correct_url_prefix.host}
 
 
 def clean_html(html: str) -> str:
---
+++ tests/unit/components/page_fetchers/test_helpers.py
@@ -0,0 +1,54 @@
+import re
+
+from article_rec_db.models.article import Language
+from pydantic import HttpUrl
+
+from article_rec_training_job.components.page_fetchers.helpers import (
+    build_url,
+    clean_url,
+    extract_slug_from_url,
+    infer_language_from_url,
+    remove_urls_with_invalid_prefix,
+)
+
+
+def test_build_url():
+    url = build_url(scheme="https", host="test.com", path="/test", query="a=1&b=2", fragment="test")
+    assert url == HttpUrl("https://test.com/test?a=1&b=2#test")
+
+
+def test_clean_url():
+    url = clean_url(HttpUrl("https://test.com/test?a=1&b=2#test"))
+    assert url == HttpUrl("https://test.com/test")
+
+
+def test_infer_language_from_url():
+    patterns_language = {
+        Language.SPANISH: re.compile(r"^/es/(.+/)*$"),
+    }
+    url_es = HttpUrl("https://test.com/es/test/")
+    url_en = HttpUrl("https://test.com/test/")  # English is the fallback
+    assert infer_language_from_url(patterns_language, url_es) == Language.SPANISH
+    assert infer_language_from_url(patterns_language, url_en) == Language.ENGLISH
+
+
+def test_extract_slug_from_url():
+    pattern_slug = re.compile(r"^/es/(?P<slug>[a-zA-Z\d\-%]+)/$")
+    url = HttpUrl("https://test.com/es/article-slug/")
+    assert extract_slug_from_url(pattern_slug, url) == "article-slug"
+
+
+def test_remove_urls_with_invalid_prefix():
+    correct_url_prefix = HttpUrl("https://test.com")
+    urls = {
+        HttpUrl("https://test.com/test1"),
+        HttpUrl("https://test.com/test2"),
+        HttpUrl("https://test-wrong.com/test3"),
+    }
+    assert remove_urls_with_invalid_prefix(correct_url_prefix, urls) == {
+        HttpUrl("https://test.com/test1"),
+        HttpUrl("https://test.com/test2"),
+    }
+
+
+# Not testing clean_html() for now because it's not terribly important
