7a0d36bee0f7fc8da00882d81d7e646b817533f3
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-12-20 11:45:12 -0500

    [add] project id fixture and comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,20 @@ from google.cloud import bigquery
 
 @pytest.fixture(scope="session")
 def port_bigquery() -> int:
+    # This needs to match the port specified in compose.yaml
     return 9050
 
 
 @pytest.fixture(scope="session")
-def client_bigquery(port_bigquery) -> bigquery.Client:
+def project_id_bigquery() -> str:
+    # This needs to match the project ID specified in compose.yaml
+    return "test"
+
+
+@pytest.fixture(scope="session")
+def client_bigquery(port_bigquery, project_id_bigquery) -> bigquery.Client:
     return bigquery.Client(
-        "test",
+        project_id_bigquery,
         credentials=AnonymousCredentials(),
         client_options=ClientOptions(api_endpoint=f"http://0.0.0.0:{port_bigquery}"),
     )
